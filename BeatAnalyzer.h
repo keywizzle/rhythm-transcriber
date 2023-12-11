8d6c0f6f21da4f245300c85858b5a5cb34122b9a
Kyle Bruce <kbsoccer00@gmail.com>
2023-12-11 16:34:21 -0500

    - Fixed many issues with branch creation - Branch score calculation moved here for more contextual scores - Significantly adjusted scores/weights

--- BeatAnalyzer.h
+++ BeatAnalyzer.h
@@ -9,6 +9,8 @@
 
 namespace RhythmTranscriber
 {
+    extern unsigned int iters;
+
     class BeatAnalyzer
     {
     public:
@@ -19,28 +21,44 @@ namespace RhythmTranscriber
 
         unsigned int maxDepth = 3;
 
-        /// Treating `beatData` as a linear tree, represents the number of sibling nodes per child
-        /// level (TODO remove)
-        std::vector<unsigned int> dataLens;
-
-        std::vector<Beat> beats;
+        /// @todo Move to private, it's public right now for testing purposes.
+        BeatBranch branch;
 
         BeatBranch bestBranch;
         float bestBranchScore = 0.f;
 
-        void analyze(BaseNote *notes, unsigned int notesLen);
+        void set_bpm(float bpm);
 
-        void try_bpm(float bpm);
+        void set_depth(unsigned int depth);
 
         void get_best_branch_at(unsigned int noteIndex);
+
+        float calc_branch_score();
 
     private:
         float expectedBeatDuration = 0.f;
         float minBeatDuration = 0.f;
         float maxBeatDuration = 0.f;
 
-        BeatBranch branch;
+        /// @brief After evaluating the best branch, this will be set to the branches first beat.
+        /// Further branch evaluations can use this as the preceding beat to the first beat of the
+        /// branch for scoring.
+        Beat recentBestBeat;
 
         void create_beat_branch(BaseNote *startNote, bool startsOffbeat, unsigned int depth);
+
+        inline void eval_branch()
+        {
+            if (branch.create_beats())
+            {
+                float branchScore = calc_branch_score();
+
+                if (branchScore > bestBranchScore)
+                {
+                    bestBranchScore = branchScore;
+                    bestBranch = branch;
+                }
+            }
+        }
     };
 }
